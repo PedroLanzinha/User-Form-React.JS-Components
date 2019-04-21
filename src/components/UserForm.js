9484819230d9239c11d3e576302d24f8d0cd0893
unknown <theitalianpeter@hotmail.com>
2019-04-21 23:36:14 +0100

    steps commit

---
+++ src/components/UserForm.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react'
+
+export class UserForm extends Component {
+    state = {
+        step: 1,
+        firstName: '',
+        lastName: '',
+        email: '',
+        ocupation: '',
+        city: '',
+        bio: ''
+    }
+
+    //Proceed to next step/page
+    nextStep = () => {
+        const { step } = this.state;
+        this.setState({
+            step: step + 1
+        });
+    }
+
+        //Go back to previous step/page
+        prevStep = () => {
+            const { step } = this.state;
+            this.setState({
+                step: step - 1
+            });
+        }
+
+        //Handle Fields change
+
+        handleChange = input => e =>{
+            this.setState({[inpu]: e.target.value});
+        }
+
+  render() {
+      const{step} = this.state;
+      const{ firstName, lastName, email, ocupation, city, bio} = this.state;
+      const values = {firstName, lastName, email, ocupation, city, bio}  
+      switch(step){
+          case 1:
+            return (
+                <FormUserDetails 
+                    nextStep={this.nextStep}
+                    handleChange = {this.handleChange}
+                    values={values}
+                />
+            )
+            case 2:
+                return <h1>FormPersonalDetails</h1>
+            case 3:
+                return <h1>Confirm</h1>
+            case 4:
+                return <h1>Success</h1>
+      }
+  }
+}
+
+export default UserForm
